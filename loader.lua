656e0980e4aa1b0cd3db33ec57da200b5e4b5143
script9ii8 <afnanmubarok14@gmail.com>
2026-01-22 06:28:34 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,15 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/agussuntikfoll-ui/Afnan-lyn/main/src/afnan_hub.lua"))()
+--// AFNAN HUB LOADER (AUTO UPDATE)
+
+if getgenv().AFNAN_LOADER then return end
+getgenv().AFNAN_LOADER = true
+
+local CORE_URL = "https://raw.githubusercontent.com/agussuntikfoll-ui/Afnan-lyn/main/src/afnan_hub.lua"
+
+local ok, err = pcall(function()
+    loadstring(game:HttpGet(CORE_URL))()
+end)
+
+if not ok then
+    warn("AFNAN HUB: Gagal load core", err)
+end
+
